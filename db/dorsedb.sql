23583651522081cb0639e2d80a5cfdaa40ffda50
VittorioValent <vittorio.valent@gmail.com>
2019-04-03 15:23:44 +0200

    database aggiornato usertype

--- db/dorsedb.sql
+++ db/dorsedb.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `dorsejava` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `dorsejava`;
 -- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
 --
 -- Host: localhost    Database: dorsejava
@@ -82,4 +84,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-04-03 15:17:26
+-- Dump completed on 2019-04-03 15:22:23
