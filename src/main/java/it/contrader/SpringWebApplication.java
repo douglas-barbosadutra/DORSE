0e84e8592bd77487e3ea31dcd05803ce27923acf
GirolamoMurdaca <girolamo.murdaca@gmail.com>
2019-04-16 14:41:45 +0200

    pulizia

--- src/main/java/it/contrader/SpringWebApplication.java
+++ src/main/java/it/contrader/SpringWebApplication.java
@@ -1,20 +1,10 @@
 package it.contrader;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 
-import it.contrader.services.UserService;
-
 @SpringBootApplication
 public class SpringWebApplication {
-
-	static UserService userService;
-
-	@Autowired
-	public SpringWebApplication(UserService userService) {
-		this.userService = userService;
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWebApplication.class, args);
