dd779a6cffebe200fcf29a2b1bf921c39f564215
VittorioValent <vittorio.valent@gmail.com>
2019-05-03 11:19:36 +0200

    login?

--- src/components/login/login.component.ts
+++ src/components/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import {LoginService} from 'src/services/login.service';
+import { UserDTO } from 'src/dto/UserDTO';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  private idUtenteLocale: number;
+  constructor(private loginService: LoginService, private router: Router) { }
 
   ngOnInit() {
+
   }
 
+  login(f: NgForm): void {
+    this.loginService.login(f.value.username, f.value.password).subscribe((response) => {
+
+      if(response != null) {
+        this.idUtenteLocale = response.id;
+        console.log(this.idUtenteLocale);
+      }
+    });
+  }
 }
