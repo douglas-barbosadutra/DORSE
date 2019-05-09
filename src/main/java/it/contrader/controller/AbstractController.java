e94f0012fc3c0855fad11cefa5a3767a3476ab51
VittorioValent <vittorio.valent@gmail.com>
2019-05-09 12:03:01 +0200

    post in metodo insert

--- src/main/java/it/contrader/controller/AbstractController.java
+++ src/main/java/it/contrader/controller/AbstractController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -30,7 +31,7 @@ public abstract class AbstractController <Entity, DTO>{
 		return dto;
 	}
 	
-	@GetMapping("/insert")
+	@PostMapping("/insert")
 	public DTO insert (@RequestBody DTO dto) {
 		service.insert(dto);
 		return dto;
